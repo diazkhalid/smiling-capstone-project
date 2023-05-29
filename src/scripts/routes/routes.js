cde171492648c01eb8a1941b3c283dcfe7444a77
RacThug <82396667+RacThug@users.noreply.github.com>
2023-05-29 21:49:24 +0800

    Menambahkan halaman favorite story

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,10 +2,12 @@
 
 import Beranda from '../views/pages/beranda';
 import Detail from '../views/pages/detail';
+import Favorit from '../views/pages/favorit';
 
 const routes = {
   '/': Beranda,
   '/detail': Detail,
+  '/favorit': Favorit,
 };
 
 export default routes;
---
+++ src/scripts/views/pages/favorit.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-plusplus */
+import { createStoryItemTemplate, createStoryItemTemplate2 } from '../templates/template-creator';
+
+const Favorit = {
+  async render() {
+    return `
+        <h2 class="m-5">Buku Favorite</h2>
+        <div class="container mt-5 rounded">
+          <div class="row" id="card-container"></div>
+        </div>
+        `;
+  },
+
+  async afterRender() {
+    console.log('after render');
+    const container = document.querySelector('#card-container');
+    for (let i = 0; i < 12; i++) {
+      if (i % 2 === 0) {
+        container.innerHTML += createStoryItemTemplate();
+      } else {
+        // untuk tes overflow deskripsi
+        container.innerHTML += createStoryItemTemplate2();
+      }
+    }
+
+    // const stories = await FavoriteStoryIdb.getAllStories();
+    // const storyContainer = document.querySelector('#storyList');
+
+    // stories.forEach((story) => {
+    //   storyContainer.innerHTML += createStoryItemTemplate(story);
+    // }); untuk nanti kalo udah ada Idb
+  },
+
+};
+
+export default Favorit;
