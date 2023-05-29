57f236319036c06cee74696b4c567e741b3867d4
Fahru Alfarizi Hananza Putrawan <fahrualfarizi7@gmail.com>
2023-05-29 23:06:12 +0800

    menambahkan halaman tentang kami

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,11 +3,13 @@
 import Beranda from '../views/pages/beranda';
 import Detail from '../views/pages/detail';
 import Favorit from '../views/pages/favorit';
+import TentangKami from '../views/pages/tentang-kami';
 
 const routes = {
   '/': Beranda,
   '/detail': Detail,
   '/favorit': Favorit,
+  '/tentang-kami': TentangKami,
 };
 
 export default routes;
